927be1e8ce64fd008e6b9efdc7d47c0760a61963
Ashwani <a4ashwanik4kr@gmail.com>
2024-06-30 18:10:00 +0530

    renamed

--- print new line.py
+++
@@ -1,9 +0,0 @@
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-
-
-print("Hello world!\nHello World!\nHello World")
-
-print("Hello" + " " + "Ashwani" )
---
+++ printnewline.py
@@ -0,0 +1,13 @@
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+
+
+print("Hello world!\nHello World!\nHello World")
+
+print("Hello" + " " + "Ashwani" )
+print("Hello" + " " + "Ashwani" )
+print("Hello" + " " + "Ashwani" )
+print("Hello" + " " + "Ashwani" )
+print("Hello" + " " + "Ashwani" )
